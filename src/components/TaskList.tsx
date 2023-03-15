95f75f974005ded091545e650cd70f4f2461fadd
Suporte TI <suporte.zanaflex@ZANAN.LOCAL>
2023-03-15 14:00:12 -0300

    Third Commit

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,7 +16,6 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if(newTaskTitle === ''){
       return
     } else {
@@ -31,19 +30,17 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: string) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
     const newTask = tasks.map(task => task.id === id ? {
       ...task,
       isComplete: !task.isComplete
     }: task);
-    
+
     setTasks(newTask);
 
     
   }
 
   function handleRemoveTask(id: string) {
-    // Remova uma task da listagem pelo ID
     const filterTask = tasks.filter(task => task.id !== id);
 
     setTasks(filterTask);
